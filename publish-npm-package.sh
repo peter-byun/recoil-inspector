122c9263b875a0045710c8d34d8db287b2f1334e
Peter Byun <peter1035k@gmail.com>
2023-12-03 22:50:04 +0900

    chore update the version

---
+++ publish-npm-package.sh
@@ -0,0 +1,8 @@
+rm packages/recoil-inspector/package.json 
+cp packages/recoil-inspector/package.publish.json packages/recoil-inspector/package.json
+
+yarn install
+yarn run build --filter recoil-inspector
+
+cd packages/recoil-inspector
+npm version patch
